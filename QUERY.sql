769e30c0483fe2d2fc973dcbd1d7f282eafa3eab
torufa <torufa3@gmail.com>
2026-06-14 11:11:57 +0600

    fix(query-3): resolve case sensitivity by replacing LIKE with ILIKE

--- QUERY.sql
+++ QUERY.sql
@@ -92,11 +92,11 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 SELECT match_id, fixture, base_ticket_price FROM matches
-WHERE tournament_category = 'Champions League' and match_status = 'Available'
+WHERE tournament_category = 'Champions League' AND match_status = 'Available'
 
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 SELECT user_id, full_name, email FROM users
-WHERE full_name Like 'Tanvir%' OR full_name LIKE '%Haque'
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque'
 
 -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
 SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status FROM bookings
